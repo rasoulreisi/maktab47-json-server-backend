a0b9f56e13271b72d2a9cd3aa48006316912355a
Babak <k.babak@gmail.com>
2021-05-30 20:28:34 +0430

    add multer and upload related apis to backend script

--- server.js
+++ server.js
@@ -2,9 +2,33 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const fs = require('fs')
+const path = require('path')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.post('/upload', upload.single('image'), function (req, res, next) {
+  // req.file is the `image` file
+  // req.body will hold the text fields, if there were any
+  res.json(req.file)
+})
+
+server.get('/files', (req, res, next) => {
+  fs.readdir('./uploads/', (err, files) => {
+    if (err) return next(err)
+    res.json(files)
+  })
+})
+
+server.get('/files/:file_id', (req, res, next) => {
+  const { file_id } = req.params
+  res.set('Content-Type', 'image/jpeg')
+  res.sendFile(path.join(__dirname, 'uploads/'+file_id))
+})
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
