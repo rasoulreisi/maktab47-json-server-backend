8da9d531fe6463f9fde4c38b76631930074df948
Babak <k.babak@gmail.com>
2021-06-05 18:45:49 +0430

    explain better server.js codes

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ server.get('/files/:file_id', (req, res, next) => {
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
+
+// Add createdAt field with timestamp value when posting to any route
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now()
@@ -41,8 +43,9 @@ server.use((req, res, next) => {
   next()
 })
 
-// Use default router
+// Use default router (CRUDs of db.json)
 server.use(router)
+
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
